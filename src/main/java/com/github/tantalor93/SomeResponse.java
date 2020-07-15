790f2e9d88f364e57357576f6057f4c99b459fc6
Ondrej Benkovsky <ondrej.benkovsky@wandera.com>
2020-07-15 16:21:55 +0200

    add reflection support

--- src/main/java/com/github/tantalor93/SomeResponse.java
+++ src/main/java/com/github/tantalor93/SomeResponse.java
@@ -2,6 +2,9 @@ package com.github.tantalor93;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public class SomeResponse {
 
 	@JsonProperty
